7eff2b669aea05b4841519ab6868952f0b5f9a8a
**sujan <sujanmoi128@gmail.com>
2022-01-31 02:36:24 +0530

    fixed

--- resources/views/home/question.blade.php
+++ resources/views/home/question.blade.php
@@ -3,7 +3,6 @@
 
 <head>
     <meta charset="UTF-8">
-
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -12,129 +11,14 @@
     <link rel="stylesheet" href="{{ asset('question.css') }}">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <title>Document</title>
-    <style>
-        .savecolor {
-            background: #0cc93d !important;
-        }
-
-        .clearcolor {
-            background: #d8d8d8 !important;
-        }
-
-        .markcolor {
-            background: #ecb20e !important;
-        }
-
-        .reviewcolor {
-            background: #1b488a !important;
-        }
-
-        .bordercolor {
-            border: 2px red !important;
-        }
-
-        .question_image img {
-            width: 100%;
-
-            margin-bottom: 10px
-        }
-
-        @media screen and (max-width: 900px) {
-            .headsection {
-                height: 6rem;
-            }
-
-            .part-1 {
-                flex-direction: column;
-            }
-
-
-            .first_logo img {
-                width: 45px;
-                margin: 0px 8px;
-
-            }
-
-            .second_logo {
-                margin: 11px 2px;
-            }
-
-            .second_logo img {
-                width: 58px;
-                height: 100%;
-                margin: 0px;
-
-            }
-
-            .main-question-part {
-                height: 15rem !important;
-            }
-
-            .first_intro p {
-
-                font-size: 12px;
-
-            }
-
-            .question_no hr {
-                height: 1px;
-                margin: 10px 0;
-            }
-
-            .question_no h1 {
-                font-size: 13px;
-                padding-top: 27px;
-
-            }
-
-            .button button {
-                margin-right: 3px;
-                font-weight: bold;
-                font-size: 8px;
-            }
-
-            .button {
-                padding-left: 0px;
-            }
-
-            .answerbox-box {
-                width: 100%;
-            }
-
-            .timer-box {
-                height: 4rem;
-                background: #424141;
-
-            }
-
-            .timer-box span {
-                font-size: 31px;
-                color: red;
-            }
-
-            .option-box {
-                height: 25rem;
-
-            }
-
-            .question-box {
-                margin-left: 0 !important;
-                width: 21rem !important;
-            }
-        }
-
-    </style>
+    <title>Study POint Live</title>
     @livewireStyles
 </head>
 
 <body>
-
     <livewire:question.show-questions />
     @livewireScripts
 </body>
-
-
 
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
--- resources/views/livewire/question/show-questions.blade.php
+++ resources/views/livewire/question/show-questions.blade.php
@@ -1,40 +1,24 @@
 <div>
-    <section>
+    <section class=" bg-dark bg-gradient">
 
-        <!-- head -->
 
-        <div class="d-flex">
-            <div class="first_logo d-flex justify-content-center align-items-center">
-                <img src="{{ asset('img/logo.jpeg') }}" class="" alt="logo">
-            </div>
-            <div class="first_intro d-flex">
-                <div class="second_logo d-flex justify-content-center align-items-center ms-2">
-                    <img src="" class="" alt="logo">
+        {{-- <div class="first_logo">
+                <img src="{{ asset('img/logo.jpeg') }}" class=" img-fluid float-start w-10" alt="logo">
+            </div> --}}
+        <div class="d-flex justify-content-center py-2">
+            <div>
+                <h3 class="text-light text-center" style="">Study Point Live</h3>
+                <h6 class="ms-3 mb-0 mt-2 text-center text-light">Student ID :- <span>0254178</span></h6>
+                <div>
+                    <span class="ms-3 text-light">Subject :- <span>History</span></span>
+                    <span class="ms-3 text-light">Reamaining Time :- <span>1 hour</span></span>
                 </div>
-                <div class="student_details">
-
-                    <p class="ms-3 mb-0 mt-2">Student ID: <span></span></p>
-                    <p class="ms-3 mb-0 ">Subject Name: <span></span></p>
-                    <p class="ms-3">Reamaining Time:
-                        <span></span>
-                    </p>
-                    <div id="action_btn" data-btn="1" style="display:none;">1</div>
-                    <div id="timeing" data-btn="" style="display:none;">1</div>
-                </div>
-
-            </div>
-            <div class="last_logo">
-                {{-- <img src="img/omm.jpg" class="" alt="logo"> --}}
             </div>
         </div>
-
-        <!-- head -->
-
     </section>
 
-    <section class="container-fluid d-flex part-1" style="height: 95vh;">
-        <div class="question-box d-flex justify-content-center align-items-center ms-3"
-            style="height: 100%; width: 75%;">
+    <section class=" container d-lg-flex justify-content-between my-lg-5">
+        <div class="question-box d-flex" style="height: 100%; width: 70%;">
             <div class="main-question">
                 <div class="main-question-part" style="overflow-y: scroll; height:25rem;">
                     <div class="question_no">
@@ -43,64 +27,63 @@
                         <hr>
                     </div>
                     <div class="question_image">
-                        <img src="{{ asset('image/' . $onQuestion?->image . '') }}" class="img-fluid" alt="">
+                        <img src="{{ asset('image/' . $onQuestion?->image . '') }}" class="img-fluid w-100" alt="">
                     </div>
 
                     <table class="table table-borderless mb0">
                         <tbody>
                             <tr>
-                                <td> <input wire:model="answer" type="radio" value="1" name="radiospage01"
-                                        id="rOption01_1"> 1 ) </td>
-                                <td> <input wire:model="answer" type="radio" value="2" name="radiospage01"
-                                        id="rOption01_1"> 2 ) </td>
-                                <td> <input wire:model="answer" type="radio" value="3" name="radiospage01"
-                                        id="rOption01_1"> 3 ) </td>
-                                <td> <input wire:model="answer" type="radio" value="4" name="radiospage01"
-                                        id="rOption01_1"> 4 ) </td>
+                                <td> <input wire:model="answer" type="radio" value="1"> 1 ) </td>
+                                <td> <input wire:model="answer" type="radio" value="2"> 2 ) </td>
+                                <td> <input wire:model="answer" type="radio" value="3"> 3 ) </td>
+                                <td> <input wire:model="answer" type="radio" value="4"> 4 ) </td>
                             </tr>
                         </tbody>
                     </table>
                     <hr>
                 </div>
-                <div class="button-box">
+
+                <div class="my-2">
                     <hr>
-                    <ul class="button">
-                        <button wire:click="saveAndNext" type="button" class="btn btn-success shadow rounded">SAVE &
+                    <div class=" my-3">
+                        <button wire:click="saveAndNext" type="button" class="btn btn-success shadow rounded my-1">SAVE
+                            &
                             NEXT</button>
                         <button wire:click=" clear" type="button"
-                            class="btn btn-light text-dark shadow rounded">CLEAR</button>
+                            class="btn btn-light text-dark shadow rounded my-1">CLEAR</button>
                         <button wire:click="saveAndMarkForNext" type="button"
-                            class="btn btn-warning shadow rounded">SAVE
+                            class="btn btn-warning shadow rounded my-1">SAVE
                             & MARK FOR NEXT</button>
                         <button wire:click="markForReviewAndNext" type="button"
-                            class="btn btn-primary shadow rounded">MARK FOR REVIEW & NEXT</button>
-                    </ul>
+                            class="btn btn-primary shadow rounded my-1">MARK FOR REVIEW & NEXT</button>
+                    </div>
                 </div>
-                <div class="button-box d-flex  align-items-center"
-                    style="background: rgb(240, 240, 240); height: 60px;">
 
-                    <ul class="button  mb-0 ps-3">
+                <div class="d-flex justify-content-between bg-secondary bg-gradient shadow rounded-3 p-2">
+                    <div class="">
                         <button wire:click="findQuestion({{ $questionNo - 1 }})" type="button" id="back_btn"
-                            class="btn btn-primary" style="background: white; color: black;">
+                            class="btn btn-light">
                             << BACK </button>
                                 <button wire:click="findQuestion({{ $questionNo + 1 }})" type="button"
-                                    class="btn btn-primary" id="next_btn" style="background: white; color: black;">NEXT
+                                    class="btn btn-primary" id="next_btn">NEXT
                                     >></button>
-                    </ul>
-                    <a type="button" href="{{ route('submit') }}" class="btn btn-primary ms-auto" id="submit" style="height: 38PX;
-                   margin-right: 13px;background:#0cc93d ;"> SUBMIT
-                    </a>
+                    </div>
+                    <div>
+                        <button type="button" href="{{ route('submit') }}"
+                            class="btn btn-success shadow rounded border-dark"> SUBMIT
+                        </button>
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
 
         <div class="answerbox-box">
-            <div class="timer-box d-flex align-items-center justify-content-center">
-                <span>1: 56 : 60 </span>
+            <div class="timer-box d-flex align-items-center justify-content-center shadow rounded">
+                <span class=" ">1: 56 : 60 </span>
             </div>
 
-            <div class="option-box">
+            <div class="option-box py-3 px-2">
                 <div class="row g-3" style="margin:0px 3px">
                     @foreach ($questions as $question)
 
@@ -108,7 +91,7 @@
                             <button type="button" wire:click="findQuestion({{ $question->question_no }})"
                                 class="btn rounded shadow
                                 @if ($question->question_no == $questionNo)
-                                border-danger border-4
+                                border-danger border-3
                                 @endif
                               @if (isset($markedAnswers[$question->question_no]['color']))
                                   btn-{{ $markedAnswers[$question->question_no]['color'] }}
@@ -121,10 +104,17 @@
                 </div>
             </div>
         </div>
-
     </section>
 
-
+    <section class="bg-dark bg-gradient d-flex justify-content-between  py-3">
+        <div class="text-light px-3">Copyright © 2022 Study Point Live</div>
+        <div class="px-3">
+            <i class="fab fa-twitter text-light mx-2"></i>
+            <i class="fab fa-youtube text-light mx-2"></i>
+            <i class="fab fa-facebook text-light mx-2"></i>
+            <i class="fab fa-instagram text-light mx-2"></i>
+        </div>
+    </section>
     <!-- timer -->
     <script>
         $(function() {
